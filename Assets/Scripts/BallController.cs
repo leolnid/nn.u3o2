4cec4598502879c8d0861fa79f41b7bff0650546
Leo <leonid.dyukov@gmail.com>
2020-10-04 15:01:09 +0300

    04.10

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+  public bool IsExpanding = false;
+
+  public float SizeIncreaseSpeed = 1.009f;
+  public float SizeDecreaseSpeed = 1.005f;
+
+  public float MaxSize = 3;
+  public float MinSize = 0.1f;
+
+  public void Start()
+  {
+    StartCoroutine(_ManageBaloonSize());
+  }
+
+  public void StartExpanding()
+  {
+    IsExpanding = true;
+  }
+
+  public void StartShrinking()
+  {
+    IsExpanding = false;
+  }
+
+  private IEnumerator _ManageBaloonSize()
+  {
+    while (transform.localScale.x > MinSize)
+    {
+      if (IsExpanding)
+      {
+        if (transform.localScale.x < MaxSize)
+          transform.localScale *= SizeIncreaseSpeed;
+      }
+      else
+      {
+        transform.localScale /= SizeIncreaseSpeed;
+      }
+
+      yield return new WaitForFixedUpdate();
+    }
+
+    Destroy(gameObject);
+  }
+}
---
+++ Assets/Scripts/BallInteractor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallInteractor : MonoBehaviour
+{
+  BallController last;
+
+  void Start()
+  {
+
+  }
+
+  void FixedUpdate()
+  {
+    print(last);
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+    if (Physics.Raycast(ray, out hit))
+    {
+      last = hit.transform.parent.gameObject.GetComponent<BallController>();
+
+      if (last != null)
+      {
+        last.StartExpanding();
+      }
+    }
+    else
+    {
+      if (last != null)
+      {
+        last.StartShrinking();
+      }
+
+      last = null;
+    }
+  }
+}
